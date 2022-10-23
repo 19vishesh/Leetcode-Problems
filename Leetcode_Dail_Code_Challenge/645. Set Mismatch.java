33d30cdc9a1206e504021335276724fb4af8f7a5
Vishesh-Kumar <31867166+19vishesh@users.noreply.github.com>
2022-10-23 11:08:54 +0530

    Create 645. Set Mismatch.java

---
+++ Leetcode_Dail_Code_Challenge/645. Set Mismatch.java
@@ -0,0 +1,55 @@
+// You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+// You are given an integer array nums representing the data status of this set after the error.
+
+// Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,2,4]
+// Output: [2,3]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [1,2]
+ 
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// 1 <= nums[i] <= 104
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] res = new int[2];
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int var : nums)
+            map.put(var, map.getOrDefault(var, 0)+1);       
+        
+        for(int var : map.keySet()){
+            if(map.get(var)==2)
+                res[0] = var;
+        }
+        // int repeatedElmt = getkey(map, 2);
+        // res[0] = repeatedElmt;
+        
+        for(int i=1; i<=nums.length; i++){
+            if(!map.containsKey(i))
+                res[1] = i;
+        }
+        
+        return res;
+    }
+    
+    // Function to get KeyValue
+    public static Integer getkey(Map<Integer, Integer> map, Integer value){
+        for(Map.Entry<Integer, Integer> ent : map.entrySet()){
+            if(value.equals(ent.getValue()))
+                return ent.getKey();
+        }
+        return null;
+    }
+}
